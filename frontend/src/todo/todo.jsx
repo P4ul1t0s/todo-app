6291650e02e5906cc18e401e58d89eed27b7470a
Paulo <paulomafra2004@gmail.com>
2022-08-28 16:26:51 -0300

    Eventos add & onChange

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,26 @@ import TodoForm from "./todoForm.jsx";
 import TodoList from "./todoList.jsx";
 
 export default class Todo extends Component{
+    constructor(props){
+        super(props)
+        this.state = {description: '', list: []}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+        
+    handleAdd(){
+        console.log(this.state.description)
+    }
+
     render(){
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm></TodoForm>
+                <TodoForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange}></TodoForm>
                 <TodoList></TodoList>
             </div>
         )
