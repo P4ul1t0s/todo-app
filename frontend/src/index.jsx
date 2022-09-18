a7b0f1c4975ae347a8667c99f051fca72facebdc
Paulo <paulomafra2004@gmail.com>
2022-09-18 17:17:49 -0300

    Configurando Redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import reducers from "./main/reducers"
 import App from './main/app.jsx'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+, document.getElementById('app'))
